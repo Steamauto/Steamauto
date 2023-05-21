8cdc6140baeacb2141edf86a9ee629ecf8fad953
jiajiaxd <i@jiajiaxd.com>
2023-05-21 09:13:17 +0800

    格式化代码

--- utils/static.py
+++ utils/static.py
@@ -1,26 +1,26 @@
 import os
 
-config = {'no_pause': False}
+config = {"no_pause": False}
 
-APPRISE_ASSET_FOLDER = 'Apprise'
-LOGS_FOLDER = 'logs'
-CONFIG_FOLDER = 'config'
-CONFIG_FILE_PATH = os.path.join(CONFIG_FOLDER, 'config.json')
-BUFF_COOKIES_FILE_PATH = os.path.join(CONFIG_FOLDER, 'buff_cookies.txt')
-UU_TOKEN_FILE_PATH = os.path.join(CONFIG_FOLDER, 'uu_token.txt')
-STEAM_ACCOUNT_INFO_FILE_PATH = os.path.join(CONFIG_FOLDER, 'steam_account_info.json')
-SESSION_FOLDER = 'session'
-STEAM_SESSION_PATH = os.path.join(SESSION_FOLDER, 'steam_session.pkl')
-DEV_FILE_FOLDER = 'dev'
-BUFF_ACCOUNT_DEV_FILE_PATH = os.path.join(DEV_FILE_FOLDER, 'buff_account.json')
-MESSAGE_NOTIFICATION_DEV_FILE_PATH = os.path.join(DEV_FILE_FOLDER, 'message_notification.json')
-STEAM_TRADE_DEV_FILE_PATH = os.path.join(DEV_FILE_FOLDER, 'steam_trade.json')
-SELL_ORDER_HISTORY_DEV_FILE_PATH = os.path.join(DEV_FILE_FOLDER, 'sell_order_history.json')
-SHOP_LISTING_DEV_FILE_PATH = os.path.join(DEV_FILE_FOLDER, 'shop_listing.json')
-EXAMPLE_CONFIG_FILE_PATH = os.path.join(CONFIG_FOLDER, 'config.example.json')
-SUPPORT_GAME_TYPES = [{'game': 'csgo', 'app_id': 730}, {'game': 'dota2', 'app_id': 570}]
+APPRISE_ASSET_FOLDER = "Apprise"
+LOGS_FOLDER = "logs"
+CONFIG_FOLDER = "config"
+CONFIG_FILE_PATH = os.path.join(CONFIG_FOLDER, "config.json")
+BUFF_COOKIES_FILE_PATH = os.path.join(CONFIG_FOLDER, "buff_cookies.txt")
+UU_TOKEN_FILE_PATH = os.path.join(CONFIG_FOLDER, "uu_token.txt")
+STEAM_ACCOUNT_INFO_FILE_PATH = os.path.join(CONFIG_FOLDER, "steam_account_info.json")
+SESSION_FOLDER = "session"
+STEAM_SESSION_PATH = os.path.join(SESSION_FOLDER, "steam_session.pkl")
+DEV_FILE_FOLDER = "dev"
+BUFF_ACCOUNT_DEV_FILE_PATH = os.path.join(DEV_FILE_FOLDER, "buff_account.json")
+MESSAGE_NOTIFICATION_DEV_FILE_PATH = os.path.join(DEV_FILE_FOLDER, "message_notification.json")
+STEAM_TRADE_DEV_FILE_PATH = os.path.join(DEV_FILE_FOLDER, "steam_trade.json")
+SELL_ORDER_HISTORY_DEV_FILE_PATH = os.path.join(DEV_FILE_FOLDER, "sell_order_history.json")
+SHOP_LISTING_DEV_FILE_PATH = os.path.join(DEV_FILE_FOLDER, "shop_listing.json")
+EXAMPLE_CONFIG_FILE_PATH = os.path.join(CONFIG_FOLDER, "config.example.json")
+SUPPORT_GAME_TYPES = [{"game": "csgo", "app_id": 730}, {"game": "dota2", "app_id": 570}]
 
-DEFAULT_STEAM_ACCOUNT_JSON='''
+DEFAULT_STEAM_ACCOUNT_JSON = """
 {
   // Steam 的数字 ID（字符串格式）
   "steamid": "",
@@ -40,4 +40,4 @@ DEFAULT_STEAM_ACCOUNT_JSON='''
   // Steam 登录时填写的密码
   "steam_password": ""
 }
-'''
+"""
--- utils/tools.py
+++ utils/tools.py
@@ -1,18 +1,18 @@
 import chardet
 
 from utils.logger import logger
-from utils.static import *
+from utils.static import config
 
 
 # 用于解决读取文件时的编码问题
 def get_encoding(file_path):
     with open(file_path, "rb") as f:
         data = f.read()
-        charset = chardet.detect(data)['encoding']
+        charset = chardet.detect(data)["encoding"]
     return charset
 
 
 def pause():
-    if 'no_pause' in config and not config['no_pause']:
-        logger.info('点击回车键继续...')
+    if "no_pause" in config and not config["no_pause"]:
+        logger.info("点击回车键继续...")
         input()
