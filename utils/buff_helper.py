71ae3e7a6f64063382eaac867ab80884dec96f4f
lupohan44 <cptf_lupohan@126.com>
2023-11-10 00:02:04 -0500

    增加写了一半的BUFF二维码登录代码

--- utils/buff_helper.py
+++ utils/buff_helper.py
@@ -1,6 +1,9 @@
+import io
 import os
+import time
 from typing import Dict
 
+import qrcode
 import requests
 from bs4 import BeautifulSoup
 
@@ -32,6 +35,42 @@ def login_to_buff(steam_client: SteamClient) -> str:
     while response.status_code == 302:
         response = steam_client._session.get(response.headers['Location'], allow_redirects=False)
     return steam_client._session.cookies.get_dict(domain='buff.163.com')
+
+
+def login_to_buff_by_qrcode() -> str:
+    get_qrcode_url = 'https://buff.163.com/account/api/qr_code_login_open?_=' + str(int(time.time() * 1000))
+    response = requests.get(get_qrcode_url)
+    response_json = response.json()
+    if response_json["code"] != "OK":
+        return ""
+    cookies = response.cookies.get_dict(domain='buff.163.com')
+    qr_code_create_url = 'https://buff.163.com/account/api/qr_code_create'
+    response = requests.post(qr_code_create_url, json={'code_type': 1, 'extra_param': '{}'}, cookies=cookies).json()
+    response_json = response.json()
+    if response_json["code"] != "OK":
+        return ""
+    cookies = response.cookies.get_dict(domain='buff.163.com')
+    code_id = response_json["data"]["code_id"]
+    qr_code_url = response_json["data"]["url"]
+    qrcode.make(qr_code_url).show()
+    print("请使用手机扫描上方二维码登录BUFF")
+    status = 0
+    while status != 3:
+        time.sleep(0.01)
+        poll_url = ('https://buff.163.com/account/api/qr_code_poll?item_id=' + code_id + '&_=' +
+                    str(int(time.time() * 1000)))
+        response = requests.get(poll_url)
+        response_json = response.json()
+        if response_json["code"] != "OK":
+            return ""
+        status = response_json["data"]["state"]
+    csrf_token = response.cookies.get_dict(domain='buff.163.com')["csrf_token"]
+    response = requests.post('https://buff.163.com/account/api/qr_code_login', json={'item_id': code_id},
+                             headers={'Referer': 'https://buff.163.com/', 'X-CSRFToken': csrf_token},
+                             cookies=response.cookies)
+    print(response.json()) # TODO: 永远都是二维码过期????
+    cookies = response.cookies.get_dict(domain='buff.163.com')
+    return cookies["session"]
 
 
 def is_session_has_enough_permission(session: str) -> bool:
