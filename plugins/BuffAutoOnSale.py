dbb7f30c37d1bccfd2f12a8f1f772726ec528df1
lupohan44 <cptf_lupohan@126.com>
2024-01-05 21:41:44 -0500

    修正误删休眠30秒

--- plugins/BuffAutoOnSale.py
+++ plugins/BuffAutoOnSale.py
@@ -344,7 +344,7 @@ class BuffAutoOnSale:
 
     def exec(self):
         self.logger.info("[BuffAutoOnSale] BUFF自动上架插件已启动, 休眠30秒, 与自动接收报价插件错开运行时间")
-        # time.sleep(30)
+        time.sleep(30)
         try:
             self.logger.info("[BuffAutoOnSale] 正在准备登录至BUFF...")
             with open(BUFF_COOKIES_FILE_PATH, "r", encoding=get_encoding(BUFF_COOKIES_FILE_PATH)) as f:
