832f7304f743bbb54672a6924a5df83c7c7d73fc
jiajiaxd <i@jiajiaxd.com>
2024-07-26 12:22:22 +0800

    build_info时区修改为北京

--- utils/add_build_info.py
+++ utils/add_build_info.py
@@ -1,11 +1,14 @@
 import datetime
 import platform
 
+import pytz
+
+asia = pytz.timezone("Asia/Shanghai")
 with open("utils/static.py", "r+",encoding='utf-8') as f:
     static = f.read()
     static = static.replace(
         "正在使用源码运行",
-        f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} on {platform.system()} {platform.release()}({platform.version()})',
+        f'{datetime.datetime.now(asia).strftime("%Y-%m-%d %H:%M:%S")} on {platform.system()} {platform.release()}({platform.version()})',
     )
     f.seek(0)
     f.truncate()
