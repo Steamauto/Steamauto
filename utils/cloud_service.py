281084a9ef2601162baffcb5854520ee65595fde
jiajiaxd <i@jiajiaxd.com>
2025-04-17 17:08:03 +0800

    修复无法正常识别是否为源码运行

--- utils/cloud_service.py
+++ utils/cloud_service.py
@@ -171,7 +171,7 @@ def getAds():
         for ad in ads:
             if ad.get('stop', 0):
                 print(f'{parseBroadcastMessage(ad["message"])}\n(滞留 {ad["stop"]} 秒)\n')
-                if not hasattr(os, "_MEIPASS"):
+                if not hasattr(sys, "_MEIPASS"):
                     print('源码模式运行, 不进行暂停')
                 else:
                     time.sleep(ad['stop'])
